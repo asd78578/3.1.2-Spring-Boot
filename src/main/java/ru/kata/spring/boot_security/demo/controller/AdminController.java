35d00f2619873398029cc7fdce2606794d972163
tricky <alavutdin055@gmail.com>
2025-09-28 22:52:38 +0300

    Initial commit3

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,90 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
+import ru.kata.spring.boot_security.demo.service.RoleServic;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final UserService userService;
+    private final RoleServic roleService;
+
+    @Autowired
+    public AdminController(UserService userService, RoleServic roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+
+    @GetMapping
+    public String adminPage(Model model) {
+        model.addAttribute("users", userService.getUsersList());
+        return "admin";
+    }
+
+    @GetMapping("/{id}/page")
+    public String getUserById(Model model, @PathVariable("id") Integer id) {
+        model.addAttribute("user", userService.getUser(id));
+        return "page";
+    }
+
+    @GetMapping("/new")
+    public String createNewUser(Model model, @ModelAttribute("user") User user) {
+        model.addAttribute("user", new User());
+        model.addAttribute("allRoles", roleService.getAllRoles());
+        return "new";
+    }
+
+    @PostMapping
+    public String addUser(@ModelAttribute("user") @Valid User user
+            , BindingResult bindingResult,
+                          Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("allRoles", roleService.getAllRoles());
+            return "new";
+        }
+        userService.addUser(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editUser(Model model, @PathVariable("id") Integer id) {
+        User user = userService.getUser(id);
+        model.addAttribute("user", user);
+        model.addAttribute("allRoles", roleService.getAllRoles());
+        return "edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String updateUser(@PathVariable("id") Integer id,
+                             @ModelAttribute("user") @Valid User user,
+                             BindingResult bindingResult,
+                             Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("allRoles", roleService.getAllRoles());
+            return "edit";
+        }
+
+        // Устанавливаем ID для обеспечения обновления существующей записи
+        user.setId(id);
+        userService.editUser(user);
+        return "redirect:/admin";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable("id") Integer id) {
+        userService.deleteUser(id);
+        return "redirect:/admin";
+    }
+}
+
---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImpl.java
@@ -0,0 +1,53 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class RoleServiceImpl implements RoleServic{
+    private final RoleRepository roleRepository;
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Role> getAllRoles() {
+        return roleRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Role getRoleById(Integer id) {
+        return roleRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Role not found with id: " + id));
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Role getRoleByType(String roleType) {
+        return roleRepository.findByRoleType(roleType)
+                .orElseThrow(() -> new RuntimeException("Role not found with type: " + roleType));
+    }
+
+    @Override
+    public void saveRole(Role role) {
+        roleRepository.save(role);
+    }
+
+    @Override
+    public void deleteRole(Integer id) {
+        roleRepository.deleteById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Role> getRolesByTypes(List<String> roleTypes) {
+        return roleRepository.findByRoleTypeIn(roleTypes);
+    }
+
+}
